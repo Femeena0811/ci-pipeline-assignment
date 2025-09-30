d000150e7ea58c1357a9c7b82d410180e7b57daa
Femeena <femeenajeseer8@gmail.com>
2025-09-30 12:14:28 +0530

    First commit: adding math function and test

---
+++ math.js
@@ -0,0 +1,7 @@
+﻿// math.js
+function add(a, b) {
+  return a + b;
+}
+
+// This makes the function available for other files (like our test) to use.
+module.exports = add;
---
+++ math.test.js
@@ -0,0 +1,6 @@
+﻿// math.test.js
+const add = require('./math'); // Import the function from math.js
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
